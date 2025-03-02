184e2516d7e7e25592c8c72f733c654e1339e5ae
Kevin Cho <kevcho529@gmail.com>
2025-03-02 01:32:49 -0800

    Modified main and deleted window.py

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from window import Window
+from graphics import Window, Point, Line
+from cell import Cell
 
 
 def main():
     win = Window(800, 600)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.has_bottom_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_left_wall = False
+    c.has_bottom_wall = False
+    c.draw(100, 50, 150, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.has_bottom_wall = False
+    c.draw(50, 100, 100, 150)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.has_left_wall = False
+    c.draw(100, 100, 150, 150)
+
     win.wait_for_close()
 
 
